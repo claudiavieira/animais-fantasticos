219c144e49e6b36a98a886c2b35c9a7b7e6260ed
claudia <claudia.araujo16@gmail.com>
2022-07-28 22:26:09 -0300

    refatoracao com class do scroll anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,29 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    // saber distancia de cada elemento do topo
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
+      // console.log(isSectionVisible);
+      // console.log(sectionTop);
+      if (isSectionVisible) {
+        // console.log('animar');
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+  }
+}
