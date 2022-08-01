eb2cd75dc2fd25b3d5ae633e42222321af20a443
claudia <claudia.araujo16@gmail.com>
2022-08-01 18:01:40 -0300

    Acertado o caminho da api

--- js/script.js
+++ js/script.js
@@ -39,5 +39,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
